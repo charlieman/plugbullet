2c788d13d3449a2981c2788678d921d4532b81b4
Carlos Zúñiga <carlos.zun@gmail.com>
2009-09-29 23:21:20 -0500

    fix on url method parameters

--- bullet/urls.py
+++ bullet/urls.py
@@ -5,13 +5,16 @@ urlpatterns = patterns('bullet.views',
     #(r'^bullet/$', 'index'  ),
     (r'^registrar/$', 'register'),
     (r'^evento/(.*)/$', 'show_event'),
+
     url(r'^list/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
         archive_month,
-        {'template': 'bullet/list.html', date_field='start_date'},
-        'bullet-list'),
+        {'template': 'bullet/list.html', 'date_field': 'start_date'},
+        name='bullet-list'),
+
     url(r'^calendar/(?P<year>\d{4})/(?P<month>\w{3})/$',
-        archive_month, 
+        archive_month,
         {'template': 'bullet/calendar.html'},
-        'bullet-calendar'),
+        name='bullet-calendar'),
+
     (r'^widget/$', 'widget'),
 )
