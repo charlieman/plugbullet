38f4b6909cc4cf7709fa763ec928e04d8cbd4c4b
Carlos Zúñiga <carlos.zun@gmail.com>
2009-10-23 18:05:13 -0500

    several changes

--- bullet/views.py
+++ bullet/views.py
@@ -2,38 +2,53 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.views.generic.simple import direct_to_template
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from bullet import models
+from bullet.forms import EventRegistrationForm
 
-def index():
+def event_list(request):
+    events = models.Event.objects.month_events(from_today=True)
+    variables = {
+        'events': events,
+    }
+    return render_to_response('bullet/events_list.html', variables)
+
+def create(request):
+    if request.method == 'POST':
+        form = EventRegistrationForm(data=request.POST)
+    form = EventRegistrationForm()
+    variables = {
+        'form': form,
+    }
+    return render_to_response('bullet/create.html', variables)
+
+def show_event(request):
     pass
 
-def register():
+def list(request, year, day):
     pass
 
-def show_event():
+def calendar(request, year, day):
     pass
 
-def list(year, day):
-    pass
-
-def calendar(year, day):
-    pass
-
-def widget(request):
+def direct_json(request):
     from django.core import serializers
-    objects = models.Event.objects.comming_soon()
-    json = serializers.serialize('json', [objects], ensure_ascii=False)
+    objects = models.Event.objects.month_events(from_today=True)
+    json = serializers.serialize('json', objects, ensure_ascii=False)
     json = 'printWidget(%s);' % json
     return HttpResponse(json, mimetype='text/javascript')
 
-def noscript(request):
-    objects = models.Event.objects.comming_soon()
-    return direct_to_template(request, 'bullet/noscript.html',
-                              {'objects': [objects]})
+def widget(request, template='bullet/iframe.html'):
+    events = models.Event.objects.month_events(from_today=True)
+    variables = {
+        'events': events,
+    }
+    return render_to_response(template, variables)
 
 # helper functions
 
-def filterls(locals):
+def _filterls(locals):
     """Use this to directly pull local variables from locals() in the view
     to the template while strip unwanted variables (those that start with
     '_'), like this:
