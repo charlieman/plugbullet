996df65b9d941f633ef7a8004096d579be3c4e05
Carlos Zúñiga <carlos.zun@gmail.com>
2009-10-23 20:07:43 -0500

    added register_page view

--- main_site/views.py
+++ main_site/views.py
@@ -2,12 +2,32 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from main_site.forms import RegistrationForm
 
 def main_page(request):
     variables = RequestContext(request, {
     })
     return render_to_response('main_site/main_page.html', variables)
+
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+        'form': form,
+    })
+    return render_to_response('registration/register.html', variables)
 
